0c9e58cb5a1fed66225d30eaeccffff4b28b24d6
Lucas Petherbridge <lukexor@gmail.com>
2020-05-17 09:57:57 -0700

    Starting rewrite

---
+++ src/color.rs
@@ -0,0 +1,12 @@
+//! # Color
+//!
+//! Represents a RGB, HSB, or HSL color that can be painted to a screen.
+//!
+//! TODO
+//!
+
+/// A type representing a RGB, HSB, or HSL color
+///
+/// TODO
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Color {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,16 @@
-use std::{error, fmt};
+#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-pub mod event;
-pub mod image;
-pub mod pixel;
+//! # Pix-Engine
+//!
+//! ## Summary
+//!
+//! A simple, cross-platform graphics/UI engine framework with a minimal interface.
+//!
+//! TODO
+//!
+//! ## Usage
+//!
+//! TODO
+//!
 
-mod audio;
-mod driver;
-mod engine;
-mod state;
-
-pub use engine::PixEngine;
-pub use image::Image;
-pub use state::{draw, transform, AlphaMode, State, StateData, WindowId};
-
-pub type PixEngineResult<T> = std::result::Result<T, PixEngineErr>;
-
-pub struct PixEngineErr {
-    description: String,
-}
-
-impl PixEngineErr {
-    pub fn new<D: ToString>(desc: D) -> Self {
-        Self {
-            description: desc.to_string(),
-        }
-    }
-}
-
-impl fmt::Display for PixEngineErr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.description)
-    }
-}
-
-impl fmt::Debug for PixEngineErr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{{ err: {}, file: {}, line: {} }}",
-            self.description,
-            file!(),
-            line!(),
-        )
-    }
-}
-
-impl error::Error for PixEngineErr {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        None
-    }
-}
-impl From<std::io::Error> for PixEngineErr {
-    fn from(err: std::io::Error) -> Self {
-        Self::new(err)
-    }
-}
-impl From<String> for PixEngineErr {
-    fn from(err: String) -> Self {
-        Self::new(err)
-    }
-}
+pub mod color;
