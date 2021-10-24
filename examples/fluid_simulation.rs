9a50357c1264f4554ca23e7d4f3baf0c791b8b02
Lucas Petherbridge <lukexor@gmail.com>
2021-10-24 13:35:05 -0700

    Added parallel calculation to fluid_simulation

--- examples/fluid_simulation.rs
+++ examples/fluid_simulation.rs
@@ -1,21 +1,19 @@
 use pix_engine::prelude::*;
+use rayon::prelude::*;
 
-const WIDTH: u32 = 300;
-const HEIGHT: u32 = 300;
-const SCALE: u32 = 1;
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
 
-const N: usize = (WIDTH / SCALE) as usize;
+const N: usize = WIDTH as usize;
 const NLEN: usize = N - 1;
 const N_SCALAR: Scalar = N as Scalar;
-const ITER: usize = 2;
 
-const VEL: Scalar = 1.4; // Velocity of fluid
-const TIME_INC: Scalar = 0.5; // Amount to step time each draw
+const VEL: Scalar = 1.8; // Velocity of fluid
 
-const SPACING: usize = 20;
-const COUNT: usize = (WIDTH / SCALE) as usize / SPACING + 1;
+const SPACING: usize = 25;
+const COUNT: usize = N / SPACING + 1;
 
-const DT: Scalar = 0.005;
+const DT: Scalar = 0.003;
 const DTX: Scalar = DT * (N_SCALAR - 2.0);
 const DTY: Scalar = DT * (N_SCALAR - 2.0);
 const DIFF: Scalar = 0.00002; // Diffusion
@@ -28,6 +26,7 @@ struct Fluid {
     vely: Vec<Scalar>,
     velx0: Vec<Scalar>,
     vely0: Vec<Scalar>,
+    tmp: Vec<Scalar>,
 }
 
 fn get_idx(x: usize, y: usize) -> usize {
@@ -36,47 +35,57 @@ fn get_idx(x: usize, y: usize) -> usize {
     x + y * N
 }
 
-fn diffuse(b: usize, xs: &mut [Scalar], xs0: &[Scalar], amt: Scalar) {
-    let a = DT * amt * (N - 2).pow(2) as Scalar;
-    linear_solve(b, xs, xs0, a, 1.0 + 6.0 * a);
+fn get_xy(idx: usize) -> (usize, usize) {
+    (idx % N, idx / N)
 }
 
-fn project(velx: &mut [Scalar], vely: &mut [Scalar], p: &mut [Scalar], div: &mut [Scalar]) {
-    for j in 1..NLEN {
-        for i in 1..NLEN {
-            let idx = get_idx(i, j);
-            div[idx] = -0.5
-                * (velx[get_idx(i + 1, j)] - velx[get_idx(i - 1, j)] + vely[get_idx(i, j + 1)]
-                    - vely[get_idx(i, j - 1)])
-                / N_SCALAR;
-            p[idx] = 0.0;
-        }
-    }
-    set_bounds(0, div);
-    set_bounds(0, p);
-    linear_solve(0, p, div, 1.0, 6.0);
+fn diffuse(b: usize, xs: &mut [Scalar], xs0: &[Scalar], amt: Scalar, tmp: &mut [Scalar]) {
+    let a = DT * amt * (N - 2).pow(2) as Scalar;
+    linear_solve(b, xs, xs0, a, 1.0 + 6.0 * a, tmp);
+}
 
-    for j in 1..NLEN {
-        for i in 1..NLEN {
-            let idx = get_idx(i, j);
-            velx[idx] -= 0.5 * (p[get_idx(i + 1, j)] - p[get_idx(i - 1, j)]) * N_SCALAR;
-            vely[idx] -= 0.5 * (p[get_idx(i, j + 1)] - p[get_idx(i, j - 1)]) * N_SCALAR;
-        }
-    }
-    set_bounds(1, velx);
-    set_bounds(2, vely);
+fn project(
+    velx: &mut [Scalar],
+    vely: &mut [Scalar],
+    p: &mut [Scalar],
+    div: &mut [Scalar],
+    tmp: &mut [Scalar],
+) {
+    let c = 1.0 / 6.0;
+    div.par_iter_mut()
+        .zip(tmp.par_iter_mut())
+        .enumerate()
+        .for_each(|(i, (div, tmp))| {
+            let (x, y) = get_xy(i);
+            if (1..NLEN).contains(&x) && (1..NLEN).contains(&y) {
+                *div = -0.5
+                    * (velx[get_idx(x + 1, y)] - velx[get_idx(x - 1, y)] + vely[get_idx(x, y + 1)]
+                        - vely[get_idx(x, y - 1)])
+                    / N_SCALAR;
+                *tmp = *div * c;
+            }
+        });
+    p.swap_with_slice(tmp);
+    set_bounds(0, p);
+
+    velx.par_iter_mut()
+        .zip(vely.par_iter_mut())
+        .enumerate()
+        .for_each(|(i, (velx, vely))| {
+            let (x, y) = get_xy(i);
+            if (1..NLEN).contains(&x) && (1..NLEN).contains(&y) {
+                *velx -= 0.5 * (p[get_idx(x + 1, y)] - p[get_idx(x - 1, y)]) * N_SCALAR;
+                *vely -= 0.5 * (p[get_idx(x, y + 1)] - p[get_idx(x, y - 1)]) * N_SCALAR;
+            }
+        });
 }
 
 fn advect(b: usize, d: &mut [Scalar], d0: &[Scalar], velx: &[Scalar], vely: &[Scalar]) {
-    let (mut i0, mut i1, mut j0, mut j1);
-
-    let (mut s0, mut s1, mut t0, mut t1);
-
-    for j in 1..NLEN {
-        for i in 1..NLEN {
-            let idx = get_idx(i, j);
-            let mut x = i as Scalar - (DTX * velx[idx]);
-            let mut y = j as Scalar - (DTY * vely[idx]);
+    d.par_iter_mut().enumerate().for_each(|(i, d)| {
+        let (x, y) = get_xy(i);
+        if (1..NLEN).contains(&x) && (1..NLEN).contains(&y) {
+            let mut x = x as Scalar - (DTX * velx[i]);
+            let mut y = y as Scalar - (DTY * vely[i]);
 
             if x < 0.5 {
                 x = 0.5;
@@ -84,52 +93,61 @@ fn advect(b: usize, d: &mut [Scalar], d0: &[Scalar], velx: &[Scalar], vely: &[Sc
             if x > N_SCALAR + 0.5 {
                 x = N_SCALAR + 0.5;
             }
-            i0 = x.floor() as usize;
-            i1 = i0 + 1;
+            let i0 = x.floor() as usize;
+            let i1 = i0 + 1;
             if y < 0.5 {
                 y = 0.5;
             }
             if y > N_SCALAR + 0.5 {
                 y = N_SCALAR + 0.5;
             }
-            j0 = y.floor() as usize;
-            j1 = j0 + 1;
+            let j0 = y.floor() as usize;
+            let j1 = j0 + 1;
 
-            s1 = x - i0 as Scalar;
-            s0 = 1.0 - s1;
-            t1 = y - j0 as Scalar;
-            t0 = 1.0 - t1;
+            let s1 = x - i0 as Scalar;
+            let s0 = 1.0 - s1;
+            let t1 = y - j0 as Scalar;
+            let t0 = 1.0 - t1;
 
-            let pd = d[idx].clamp(0.0, 500.0);
-            d[idx] = s0 * (t0 * d0[get_idx(i0, j0)] + t1 * d0[get_idx(i0, j1)])
+            let pd = d.clamp(0.0, 500.0);
+            *d = s0 * (t0 * d0[get_idx(i0, j0)] + t1 * d0[get_idx(i0, j1)])
                 + s1 * (t0 * d0[get_idx(i1, j0)] + t1 * d0[get_idx(i1, j1)]);
-            d[idx] = d[idx].clamp(pd - 150.0, 500.0);
+            *d = d.clamp(pd - 150.0, 500.0);
         }
-    }
+    });
     set_bounds(b, d);
 }
 
-fn linear_solve(b: usize, xs: &mut [Scalar], xs0: &[Scalar], a: Scalar, c: Scalar) {
+#[allow(clippy::many_single_char_names)]
+fn linear_solve(
+    b: usize,
+    xs: &mut [Scalar],
+    xs0: &[Scalar],
+    a: Scalar,
+    c: Scalar,
+    tmp: &mut [Scalar],
+) {
     let c_recip = c.recip();
-    for _ in 0..ITER {
-        for j in 1..NLEN {
-            for i in 1..NLEN {
-                let idx = get_idx(i, j);
-                xs[idx] = (xs0[idx]
-                    + a * (xs[get_idx(i + 1, j)]
-                        + xs[get_idx(i - 1, j)]
-                        + xs[get_idx(i, j + 1)]
-                        + xs[get_idx(i, j - 1)]))
+    for _ in 0..2 {
+        tmp.par_iter_mut().enumerate().for_each(|(i, tmp)| {
+            let (x, y) = get_xy(i);
+            if (1..NLEN).contains(&x) && (1..NLEN).contains(&y) {
+                *tmp = (xs0[i]
+                    + a * (xs[get_idx(x + 1, y)]
+                        + xs[get_idx(x - 1, y)]
+                        + xs[get_idx(x, y + 1)]
+                        + xs[get_idx(x, y - 1)]))
                     * c_recip;
             }
-        }
+        });
     }
+    xs.swap_with_slice(tmp);
     set_bounds(b, xs);
 }
 
 fn set_bounds(b: usize, xs: &mut [Scalar]) {
-    // Top and bottom
     for i in 1..NLEN {
+        // Top and bottom
         if b == 2 {
             xs[get_idx(i, 0)] = -xs[get_idx(i, 1)];
             xs[get_idx(i, N - 1)] = -xs[get_idx(i, N - 2)];
@@ -137,15 +155,13 @@ fn set_bounds(b: usize, xs: &mut [Scalar]) {
             xs[get_idx(i, 0)] = xs[get_idx(i, 1)];
             xs[get_idx(i, N - 1)] = xs[get_idx(i, N - 2)];
         }
-    }
-    // left and right
-    for j in 1..NLEN {
+        // left and right
         if b == 1 {
-            xs[get_idx(0, j)] = -xs[get_idx(1, j)];
-            xs[get_idx(N - 1, j)] = -xs[get_idx(N - 2, j)];
+            xs[get_idx(0, i)] = -xs[get_idx(1, i)];
+            xs[get_idx(N - 1, i)] = -xs[get_idx(N - 2, i)];
         } else {
-            xs[get_idx(0, j)] = xs[get_idx(1, j)];
-            xs[get_idx(N - 1, j)] = xs[get_idx(N - 2, j)];
+            xs[get_idx(0, i)] = xs[get_idx(1, i)];
+            xs[get_idx(N - 1, i)] = xs[get_idx(N - 2, i)];
         }
     }
 
@@ -165,18 +181,20 @@ impl Fluid {
             vely: vec![0.0; count],
             velx0: vec![0.0; count],
             vely0: vec![0.0; count],
+            tmp: vec![0.0; count],
         }
     }
 
     fn step(&mut self) {
-        diffuse(1, &mut self.velx0, &self.velx, VISC);
-        diffuse(2, &mut self.vely0, &self.vely, VISC);
+        diffuse(1, &mut self.velx0, &self.velx, VISC, &mut self.tmp);
+        diffuse(2, &mut self.vely0, &self.vely, VISC, &mut self.tmp);
 
         project(
             &mut self.velx0,
             &mut self.vely0,
             &mut self.velx,
             &mut self.vely,
+            &mut self.tmp,
         );
 
         advect(1, &mut self.velx, &self.velx0, &self.velx0, &self.vely0);
@@ -187,24 +205,21 @@ impl Fluid {
             &mut self.vely,
             &mut self.velx0,
             &mut self.vely0,
+            &mut self.tmp,
         );
 
-        diffuse(0, &mut self.s, &self.density, DIFF);
+        diffuse(0, &mut self.s, &self.density, DIFF, &mut self.tmp);
         advect(0, &mut self.density, &self.s, &self.velx, &self.vely);
     }
 
     #[allow(clippy::many_single_char_names)]
     fn on_update(&mut self, s: &mut PixState) -> PixResult<()> {
         self.step();
-        let scale = SCALE as i32;
-        for i in 1..NLEN {
-            for j in 1..NLEN {
-                let x = i as i32 * scale;
-                let y = j as i32 * scale;
-                let idx = get_idx(i, j);
-
+        for i in 0..N * N {
+            let (x, y) = get_xy(i);
+            if (15..WIDTH as usize - 15).contains(&x) && (15..HEIGHT as usize - 15).contains(&y) {
                 // Draw density
-                let d = self.density[idx];
+                let d = self.density[i];
                 let m = d / 100.0;
                 let f = m * d;
                 if f > 10.0 {
@@ -213,7 +228,7 @@ impl Fluid {
                         (f / 6.0).floor() as u8,
                         (f / 16.0).floor() as u8,
                     ));
-                    s.square([x, y, scale])?;
+                    s.square([x as i32, y as i32, 1])?;
                 }
             }
         }
@@ -222,8 +237,6 @@ impl Fluid {
 
     fn add_density(&mut self, idx: usize, amount: Scalar) {
         self.density[idx] += amount;
-        let velx = random!(-VEL, VEL);
-        self.add_velocity(idx, velx, -0.06);
     }
 
     fn add_velocity(&mut self, idx: usize, amount_x: Scalar, amount_y: Scalar) {
@@ -234,30 +247,40 @@ impl Fluid {
 
 struct App {
     fluid: Fluid,
-    t: Scalar,
+    sincos: Vec<(Scalar, Scalar)>,
     xs: [Scalar; COUNT],
     ys: [Scalar; COUNT],
 }
 
 impl App {
     fn new() -> Self {
+        let mut sincos = Vec::with_capacity(628);
+        for i in 0..628 {
+            sincos.push((i as Scalar * 0.01).sin_cos());
+        }
         Self {
             fluid: Fluid::new(),
-            t: 0.0,
+            sincos,
             xs: [0.0; COUNT],
             ys: [0.0; COUNT],
         }
     }
 
-    fn flame_on(&mut self, _s: &mut PixState) -> PixResult<()> {
+    fn flame_on(&mut self) -> PixResult<()> {
         for k in 0..COUNT {
-            for i in -9..=8 {
-                for j in -8..=2 {
+            let xmin = random!(-12, -9);
+            let xmax = random!(9, 12);
+            for i in xmin..xmax {
+                let ymin = random!(-20, -5);
+                for j in ymin..0 {
                     let idx = get_idx(
                         (self.xs[k] + i as Scalar).floor() as usize,
                         (self.ys[k] + j as Scalar).floor() as usize,
                     );
-                    self.fluid.add_density(idx, random!(20.0));
+                    self.fluid.add_density(idx, random!(5.0, 20.0));
+                    let velx = random!(-VEL, VEL);
+                    let vely = random!(-0.08, -0.03);
+                    self.fluid.add_velocity(idx, velx, vely);
                 }
             }
         }
@@ -266,15 +289,17 @@ impl App {
 
     fn drag(&mut self, s: &mut PixState) -> PixResult<()> {
         let m = s.mouse_pos();
-        let r = 4.0;
-        let scale = SCALE as i32;
-        for i in 0..628 {
-            let (sin, cos) = (i as Scalar * 0.01).sin_cos();
-            let idx = get_idx(
-                ((m.x() / scale) as Scalar + r * cos).floor() as usize,
-                ((m.y() / scale) as Scalar + r * sin).floor() as usize,
-            );
-            self.fluid.add_density(idx, random!(500.0));
+        let mx = m.x() as Scalar;
+        let my = m.y() as Scalar;
+        for r in 3..10 {
+            let r = r as Scalar;
+            for (sin, cos) in self.sincos.iter() {
+                let idx = get_idx((mx + r * cos) as usize, (my + r * sin) as usize);
+                self.fluid.add_density(idx, random!(2.0, 5.0));
+                let velx = random!(-VEL / 3.0, VEL / 3.0);
+                let vely = random!(-0.02, -0.01);
+                self.fluid.add_velocity(idx, velx, vely);
+            }
         }
         Ok(())
     }
@@ -285,6 +310,8 @@ impl AppState for App {
         s.background(BLACK)?;
         s.rect_mode(RectMode::Center);
         s.no_stroke();
+        s.cursor(&Cursor::hand())?;
+        s.clip([15, 15, WIDTH - 30, HEIGHT - 30])?;
 
         for i in 0..COUNT {
             self.xs[i] = (i * SPACING) as Scalar;
@@ -298,8 +325,7 @@ impl AppState for App {
         if s.mouse_down(Mouse::Left) {
             self.drag(s)?;
         }
-        self.flame_on(s)?;
-        self.t += TIME_INC;
+        self.flame_on()?;
         self.fluid.on_update(s)?;
         Ok(())
     }
@@ -317,6 +343,7 @@ pub fn main() -> PixResult<()> {
         .with_title("Fluid Simulation")
         .position_centered()
         .with_frame_rate()
+        .vsync_enabled()
         .build();
     let mut app = App::new();
     engine.run(&mut app)
