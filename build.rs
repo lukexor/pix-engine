1be84b0fcf2bf37f6a36a3df3e42fb1ac075037f
Lucas Petherbridge <lukexor@gmail.com>
2022-06-15 07:37:22 -0700

    build: Fix build.rs

--- build.rs
+++ build.rs
@@ -1,15 +1,16 @@
 use std::env;
 use std::path::PathBuf;
 
-const MSVC: &str = "lib/msvc";
-const MINGW: &str = "lib/gnu-mingw";
+const MSVC: &str = "msvc";
+const MINGW: &str = "gnu-mingw";
 
-fn main() {
+fn main() -> Result<(), &'static str> {
     let target = env::var("TARGET").expect("valid TARGET defined");
     if target.contains("pc-windows") {
         let manifest_dir = PathBuf::from(
             env::var("CARGO_MANIFEST_DIR").expect("valid CARGO_MANIFEST_DIR defined"),
-        );
+        )
+        .join("lib");
         let mut lib_dir = manifest_dir.clone();
         let mut dll_dir = manifest_dir.clone();
         if target.contains("msvc") {
@@ -20,7 +21,7 @@ fn main() {
             dll_dir.push(MINGW);
         } else {
             eprintln!("{} target unsupported", target);
-            return;
+            return Ok(());
         }
         lib_dir.push("lib");
         dll_dir.push("dll");
@@ -32,7 +33,9 @@ fn main() {
             dll_dir.push("32");
         }
         println!("cargo:rustc-link-search=all={}", lib_dir.display());
-        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL directory") {
+        for entry in std::fs::read_dir(&dll_dir).unwrap_or_else(|err| {
+            panic!("Can't read DLL directory: {}, {}", dll_dir.display(), err)
+        }) {
             let entry_path = entry.expect("Invalid fs entry").path();
             let file_name_result = entry_path.file_name();
             let mut new_file_path = manifest_dir.clone();
@@ -40,10 +43,17 @@ fn main() {
                 let file_name = file_name.to_str().unwrap();
                 if file_name.ends_with(".dll") {
                     new_file_path.push(file_name);
-                    std::fs::copy(&entry_path, new_file_path.as_path())
-                        .expect("Can't copy from DLL directory");
+                    std::fs::copy(&entry_path, &new_file_path).unwrap_or_else(|err| {
+                        panic!(
+                            "Can't copy DLL directory: {} to {}, {}",
+                            entry_path.display(),
+                            new_file_path.display(),
+                            err
+                        )
+                    });
                 }
             }
         }
     }
+    Ok(())
 }
