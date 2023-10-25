bc208ba969c5e179c923c32fcc6b2809d17d7720
CHiPs44 <chips@chips44.fr>
2023-10-25 22:47:42 +0200

    feat: add scancode support WIP 2

--- src/event.rs
+++ src/event.rs
@@ -394,7 +394,7 @@ impl Default for Key {
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Scan {
-    // Just a copy of Key for now
+    // Copy of Key
     Backspace, Tab, Return, Escape, Space, Exclaim, Quotedbl, Hash, Dollar, Percent, Ampersand,
     Quote, LeftParen, RightParen, Asterisk, Plus, Comma, Minus, Period, Slash, Num0, Num1, Num2,
     Num3, Num4, Num5, Num6, Num7, Num8, Num9, Colon, Semicolon, Less, Equals, Greater, Question,
@@ -403,7 +403,25 @@ pub enum Scan {
     F7, F8, F9, F10, F11, F12, PrintScreen, ScrollLock, Pause, Insert, Home, PageUp, End, PageDown,
     Right, Left, Down, Up, NumLock, KpDivide, KpMultiply, KpMinus, KpPlus, KpEnter, Kp1, Kp2, Kp3,
     Kp4, Kp5, Kp6, Kp7, Kp8, Kp9, Kp0, KpPeriod, KpEquals, KpComma, LCtrl, LShift, LAlt, LGui,
-    RCtrl, RShift, RAlt, RGui, Unhandled
+    RCtrl, RShift, RAlt, RGui, 
+    // Added by CHiPs44
+    NonUsHash, Apostrophe, Grave, NonUsBackslash, Application, Power, F13, F14, F15, 
+    F16, F17, F18, F19, F20, F21, F22, F23, F24, Execute, Help, Menu, Select, Stop, Again, Undo, 
+    Cut, Copy, Paste, Find, Mute, VolumeUp, VolumeDown, KpEqualsAS400, International1, 
+    International2, International3, International4, International5, International6, International7, 
+    International8, International9, Lang1, Lang2, Lang3, Lang4, Lang5, Lang6, Lang7, Lang8, Lang9, 
+    AltErase, SysReq, Cancel, Clear, Prior, Return2, Separator, Out, Oper, ClearAgain, CrSel, ExSel, 
+    Kp00, Kp000, ThousandsSeparator, DecimalSeparator, CurrencyUnit, CurrencySubUnit, KpLeftParen, 
+    KpRightParen, KpLeftBrace, KpRightBrace, KpTab, KpBackspace, KpA, KpB, KpC, KpD, KpE, KpF, 
+    KpXor, KpPower, KpPercent, KpLess, KpGreater, KpAmpersand, KpDblAmpersand, KpVerticalBar, 
+    KpDblVerticalBar, KpColon, KpHash, KpSpace, KpAt, KpExclam, KpMemStore, KpMemRecall, 
+    KpMemClear, KpMemAdd, KpMemSubtract, KpMemMultiply, KpMemDivide, KpPlusMinus, KpClear, 
+    KpClearEntry, KpBinary, KpOctal, KpDecimal, KpHexadecimal, Mode, AudioNext, AudioPrev, 
+    AudioStop, AudioPlay, AudioMute, MediaSelect, Www, Mail, Calculator, Computer, AcSearch, 
+    AcHome, AcBack, AcForward, AcStop, AcRefresh, AcBookmarks, BrightnessDown, BrightnessUp, 
+    DisplaySwitch, KbdIllumToggle, KbdIllumDown, KbdIllumUp, Eject, Sleep, App1, App2, 
+    // Default value
+    Unhandled
 }
 
 impl Default for Scan {
